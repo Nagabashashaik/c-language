777761f26d8f0fbddaf1061bd0580f54da33e409
Nagabashashaik <nagabasha823@gmail.com>
2024-11-28 23:08:36 +0530

    ArthematicOpeartion

---
+++ arthematic_opeartions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    int a=100;
+    int b=200;
+
+    printf("a+b=%d\n",a+b);
+    printf("a-b=%d\n",a-b);
+    printf("a*b=%d\n",a*b);
+    printf("a/b=%d\n",a/b);
+
+}
